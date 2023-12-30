addb33bf7efa2df8fe56a2c2910eb2636c6f4b7d
cedlap <143384032+cedlap@users.noreply.github.com>
2023-12-30 16:56:31 +0100

    Update spells.js

--- pages/api/spells.js
+++ pages/api/spells.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 const readSpellsFromDirectory = (dirPath) => {
-  let spells = [];
+  let spellMap = {};
   try {
     const files = fs.readdirSync(dirPath);
     files.forEach(file => {
@@ -10,19 +10,31 @@ const readSpellsFromDirectory = (dirPath) => {
         const filePath = path.join(dirPath, file);
         const fileContents = fs.readFileSync(filePath, 'utf8');
         const spellData = JSON.parse(fileContents);
-        spells.push(...spellData);
+        spellData.forEach(spell => {
+          spellMap[spell.name.toLowerCase()] = spell;
+        });
       }
     });
   } catch (err) {
     console.error(`Error reading spells from directory ${dirPath}:`, err);
   }
-  return spells;
+  return spellMap;
 };
 
 export default function handler(req, res) {
   const spellsDir = path.join('/usr/src/app', 'spells');
   const customSpellsDir = path.join('/usr/src/app', 'custom-spells');
-  let spells = readSpellsFromDirectory(spellsDir);
-  spells = spells.concat(readSpellsFromDirectory(customSpellsDir));
-  res.status(200).json(spells);
+  const spellsMap = { ...readSpellsFromDirectory(spellsDir), ...readSpellsFromDirectory(customSpellsDir) };
+
+  const { spellName } = req.query;
+  if (spellName) {
+    const spell = spellsMap[spellName.toLowerCase()];
+    if (spell) {
+      res.status(200).json(spell);
+    } else {
+      res.status(404).json({ error: 'Spell not found' });
+    }
+  } else {
+    res.status(200).json(Object.values(spellsMap));
+  }
 }
