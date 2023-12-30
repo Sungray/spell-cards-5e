c0bcb8ca6b2b97f8877bc24c3405fa5d5f26554a
cedlap <143384032+cedlap@users.noreply.github.com>
2023-12-30 22:01:47 +0100

    Update config.js

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,7 +1,11 @@
 // pages/api/config.js
 export default function handler(req, res) {
+  const apiURL = process.env['5E_API'] || 'https://www.dnd5eapi.co/';
+  // Ensure the URL ends with a slash
+  const apiUrlWithSlash = apiURL.endsWith('/') ? apiURL : `${apiURL}/`;
+
   res.status(200).json({
     USE_5ETOOLS: process.env.USE_5ETOOLS,
-    API_URL: process.env['5E_API'] || 'https://www.dnd5eapi.co/'
+    API_URL: apiUrlWithSlash
   });
 }
