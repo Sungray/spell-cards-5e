e341832cbcb24a93af43a0f0ed4c304f2c6f6a1c
cedlap <143384032+cedlap@users.noreply.github.com>
2023-12-31 01:36:10 +0100

    Update spells.js

--- pages/api/spells.js
+++ pages/api/spells.js
@@ -29,28 +29,26 @@ const readSpellsFromDirectory = (dirPath) => {
 };
 
 export default function handler(req, res) {
-  console.log("Request received for spells API"); // Log that a request has been received
-
   const spellsDir = path.join('/usr/src/app', 'spells');
   const customSpellsDir = path.join('/usr/src/app', 'custom-spells');
   const spells = [...readSpellsFromDirectory(spellsDir), ...readSpellsFromDirectory(customSpellsDir)];
-  
-  console.log("Total spells found:", spells.length); // Log total number of spells
+
+  // Format the response to include count and results
+  const formattedResponse = {
+    count: spells.length,
+    results: spells
+  };
 
   const spellIndex = req.query.index;
-  console.log("Requested spell index:", spellIndex); // Log requested spell index
-
   if (spellIndex) {
     const spell = spells.find(s => s.index === spellIndex);
     if (spell) {
-      console.log("Spell found:", spell); // Log found spell
       res.status(200).json(spell);
     } else {
-      console.log("Spell not found for index:", spellIndex); // Log if not found
       res.status(404).json({ error: 'Spell not found' });
     }
   } else {
-    res.status(200).json({ count: spells.length, results: spells });
+    res.status(200).json(formattedResponse);
   }
 }
 
