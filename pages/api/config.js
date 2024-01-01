c77ac6ae7ae984d30f26641ae93b07e07484c598
Sungray <ced.lapage@gmail.com>
2024-01-01 16:11:09 +0100

    Update config.js

--- pages/api/config.js
+++ pages/api/config.js
@@ -6,6 +6,7 @@ export default function handler(req, res) {
 
   res.status(200).json({
     USE_LOCAL_FILES: process.env.USE_LOCAL_FILES,
-    API_URL: apiUrlWithSlash
+    API_URL: apiUrlWithSlash,
+    USE_DOWNLOADED_FOLDER: process.env.USE_DOWNLOADED_FOLDER
   });
 }
