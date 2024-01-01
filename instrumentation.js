811ba84120832d2a780a3d6792daf4545f4e52e4
Sungray <ced.lapage@gmail.com>
2024-01-01 11:55:22 +0100

    Create instrumentation.js

---
+++ instrumentation.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process');
+const util = require('util');
+
+const execAsync = util.promisify(exec);
+
+async function downloadSpellsData() {
+  const repoUrl = process.env.JSON_REPO;
+  if (!repoUrl) {
+    console.log('JSON_REPO environment variable is not set. Skipping download.');
+    return;
+  }
+
+  try {
+    await execAsync(`git clone --depth 1 --filter=blob:none --sparse ${repoUrl} /tmp/spells-repo`);
+    await execAsync('cd /tmp/spells-repo && git sparse-checkout init --cone');
+    await execAsync('cd /tmp/spells-repo && git sparse-checkout set data/spells');
+    await execAsync('find /tmp/spells-repo/data/spells -type f -name "*.json" -exec mv {} /usr/src/app/spells/ \\;');
+    await execAsync('rm -rf /tmp/spells-repo');
+  } catch (error) {
+    console.error('Failed to download spells data:', error);
+  }
+}
+
+if (process.env.NEXT_RUNTIME === 'nodejs') {
+  downloadSpellsData();
+}
