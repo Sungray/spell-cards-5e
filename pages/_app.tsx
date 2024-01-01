9276966a53d4f4cb271cb305f1e7d839dacefbec
Sungray <ced.lapage@gmail.com>
2024-01-01 11:59:50 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import store from '../stores/store';
-import { useEffect } from 'react';
-import downloadSpellsData from '../utils/downloadSpellsData'; // Adjust the path as necessary
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    downloadSpellsData();
-  }, []);
-
   return (
     <Provider store={store}>
       <Component {...pageProps} />
